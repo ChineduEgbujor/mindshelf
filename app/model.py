5686944165adf3f0f921af1787ee265d05eab878
ChineduEgbujor <egbujorc1517@student.laccd.edu>
2025-04-15 11:01:56 -0700

    switch to lightweight emotion model for render

--- app/model.py
+++ app/model.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 # Load once at startup
-emotion_classifier = pipeline("text-classification", model="nateraw/bert-base-uncased-emotion", top_k=None)
+emotion_classifier = pipeline("text-classification", model="bhadresh-savani/distilbert-base-uncased-emotion", top_k=None)
 
 def analyze_emotions(text: str):
     results = emotion_classifier(text)[0]
