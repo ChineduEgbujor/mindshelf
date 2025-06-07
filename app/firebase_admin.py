caf1de5362c0bef5aa61c81ac23eb618396d0fc4
ChineduEgbujor <egbujorc1517@student.laccd.edu>
2025-06-06 20:24:40 -0700

    final commit before deployment

--- app/firebase_admin.py
+++ app/firebase_admin.py
@@ -1,9 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, auth as firebase_auth
+import os, json
 
 # Only initialize once
 if not firebase_admin._apps:
-    cred = credentials.Certificate("app/moodmate-d9029-firebase-adminsdk-fbsvc-f71b42d0b6.json")
+    sa_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+    if not sa_json:
+        raise RuntimeError("FIREBASE_SERVICE_ACCOUNT environment variable not set")
+    sa_info = json.loads(sa_json)
+    cred = credentials.Certificate(sa_info)
+    #cred = credentials.Certificate("app/moodmate-d9029-firebase-adminsdk-fbsvc-f71b42d0b6.json")
     firebase_admin.initialize_app(cred)
 
 def verify_token(id_token: str):
